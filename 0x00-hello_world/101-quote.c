06a94e54e91ca4370f07dfd7e75efd5b832e1dec
getssh <gtamrat33@gmail.com>
2021-06-17 22:40:49 +0300

    less letters perline

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,7 @@
   */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+putchar('\n');
 return (1);
 }
