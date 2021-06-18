4fee9e23afd1fabe4c272b2b4d5325ce417964c8
getssh <gtamrat33@gmail.com>
2021-06-18 20:10:56 +0300

    correct files pushed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,9 +4,8 @@
 /**
  * main - print out comditional statments
  * Return: zero success
-/* more headers goes there */
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 		int n;
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,9 +4,7 @@
 /**
  * main - print conditional statments
  * Return: zero success
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+ */
 int main(void)
 {
 		int n;
