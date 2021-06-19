a39de0b190cbc61b9861cf1202c5c17cc1a293dc
getssh <gtamrat33@gmail.com>
2021-06-19 13:32:01 +0300

    compination of two nums

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - print single digit comp
+ * Return: sucess zero
  */
-
 int main(void)
 {
+	int i;
+	int j;
 
-	int i = 48, j = 49;
-
-	while (i < 58)
+	for (i = 0; i <= 8; i++)
 	{
-		while (j < 58)
-		{
-			putchar(i);
-			putchar(j);
 
-			if (i != 56 || j != 57)
+
+		for (j = i + 1; j <= 9; j++)
+		{
+
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i >= 0 && i < 8)
 			{
 				putchar(',');
 				putchar(' ');
 			}
-			j++;
+
+
 		}
-		i++;
-		j = i + 1;
+
 	}
+
 	putchar('\n');
+
 	return (0);
 }
