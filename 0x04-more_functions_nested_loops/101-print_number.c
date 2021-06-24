19b2a2ac901583d9ac7be526b8e4df5cee7d9233
getssh <gtamrat33@gmail.com>
2021-06-24 23:46:34 +0300

    var i added

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,8 +17,8 @@ void print_number(int n)
 	else
 		i = n;
 
-	if (n / 10 != 0)
-		print_number(n / 10);
+	if (i / 10 != 0)
+		print_number(i / 10);
 
-	_putchar(n % 10 + '0');
+	_putchar(i % 10 + '0');
 }
