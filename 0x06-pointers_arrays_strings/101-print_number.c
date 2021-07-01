d5bde04f95fb63c5e74c527733f618f9dcd46d7f
getssh <gtamrat33@gmail.com>
2021-07-02 00:33:00 +0300

    betty

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,19 +6,18 @@
  */
 void print_number(int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        if (n < 0)
-        {
-                _putchar('-');
-                i = -n;
-        }
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
 
-        else
-                i = n;
+	else
+		i = n;
 
-        if (i / 10 != 0)
-                print_number(i / 10);
-
-        _putchar(i % 10 + '0');
+	if (i / 10 != 0)
+		print_number(i / 10);
+	_putchar(i % 10 + '0');
 }
