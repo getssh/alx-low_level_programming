cc8e1facab741912441e554b9acd42335f08a1c0
getssh <gtamrat33@gmail.com>
2021-07-12 23:14:44 +0300

    changes

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+  * main - prints the minimum number of coins to make change
+  * @argc: count argumnets
+  * @argv: passed arguments
+  * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, res, cents;
+
+	cents = atoi(argv[1]);
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	while (cents >= 25)
+	{
+		c1 = cents / 25;
+		cents %= 25;
+	}
+	while (cents >= 10)
+	{
+		c2 = cents / 10;
+		cents %= 10;
+	}
+	while (cents >= 5)
+	{
+		c3 = cents / 5;
+		cents %= 5;
+	}
+	while (cents >= 2)
+	{
+		c4 = cents / 2;
+		cents %= 2;
+	}
+	while (cents >= 1)
+	{
+		c5 = cents / 1;
+		cents %= 1;
+	}
+	res = c1 + c2 + c3 + c4 + c5;
+	printf("%d\n", res);
+	return (0);
+}
