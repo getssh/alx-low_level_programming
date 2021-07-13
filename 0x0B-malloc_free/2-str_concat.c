b54c2ce7e8ca4c96e85e61c0a64f137566361811
getssh <gtamrat33@gmail.com>
2021-07-14 00:57:29 +0300

    null charchter taken as empty

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,11 +10,15 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, size, i, j;
 	char *str;
 
+	if (*s1 == NULL)
+		s1 = "";
+	if (*s2 == NULL)
+		s2 = "";
+
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
-	
 	size = len1 + len2 + 1;
 	str = malloc(sizeof(char) * size);
 	if (str == NULL)
