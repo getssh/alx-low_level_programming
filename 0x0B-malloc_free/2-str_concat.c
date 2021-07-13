0422915f7cca878982ff1dceaa9b57e120aed354
getssh <gtamrat33@gmail.com>
2021-07-14 00:52:34 +0300

    updated method

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,36 +1,33 @@
 #include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * str_concat - function
- *
- * @s1: value one
- * @s2: another value
- * Return: char pointer
- */
+  * str_concat - joins two strings in new allocated mem
+  * @s1: first  sring
+  * @s2: second string
+  * Return: pointer to new string
+  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s;
-	int len1, len2, index, index2;
+	int len1, len2, size, i, j;
+	char *str;
 
-	if (s1 == 0)
-		s1 = "";
-	if (s2 == 0)
-		s2 = "";
-	len1 = 0;
-	while (*(s1 + len1))
-		len1++;
-	len2 = 0;
-	while (*(s2 + len2))
-		len2++;
-	s = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s == 0)
-		return (0);
-	for (index = 0; index < len1; index++)
-		*(s + index) = *(s1 + index);
-	for (index2 = 0; index2 < len2; index2++, index++)
-		*(s + index) = *(s2 + index2);
-	*(s + (len1 + len2)) = '\0';
-	return (s);
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
+	
+	size = len1 + len2 + 1;
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
+	str[size] = '\0';
+
+	return (str);
 }
+
