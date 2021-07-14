15d423a73af07c6a01fbba426f87e28181eb0c97
getssh <gtamrat33@gmail.com>
2021-07-13 23:27:27 -0700

    trying it with commenting out free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,12 +24,15 @@ int **alloc_grid(int width, int height)
 	{
 		arr[i] = malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
-		{
+		/*
+		 * {
 			for (j = 0; j < width; j++)
 				free(arr[j]);
 			free(arr);
 			return (NULL);
 		}
+		*/
+			return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
