d6e8663eb69a54ac0a90cae0428395f0bc23bb2b
getssh <gtamrat33@gmail.com>
2021-07-15 21:30:55 +0300

    chaching len2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,17 +11,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, size, len1, len2;
 	char *str;
 
-	if (s1 == 0)
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == 0)
+	if (s2 == NULL)
 		s2 = "";
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 
-	if (n >= len2)
-		n = len2;
+	if (n < len2)
+		len2 = n;
 	size = len1 + n + 1;
 	str = malloc(sizeof(char) * size);
 
