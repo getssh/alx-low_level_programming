03c0f8e1eb902ee31d01a8ac4ea3d1075c90f1e4
getssh <gtamrat33@gmail.com>
2021-07-15 22:42:43 +0300

    back to orignal code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,16 +8,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *ptr;
+	int *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(size * nmemb);
+	ptr = malloc(sizeof(size) * nmemb);
 
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < (nmemb * size); i++)
+	for (i = 0; i < size; i++)
 		ptr[i] = 0;
 
 	return (ptr);
