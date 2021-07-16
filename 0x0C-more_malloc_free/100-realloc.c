296be89b181fc3b7e7ce32eafcaba46141c3343c
getssh <gtamrat33@gmail.com>
2021-07-16 22:58:55 +0300

    ptr NULL

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -22,13 +22,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	new = malloc(new_size);
-	if (ptr == NULL)
-	{
-		for (i = 0; i < new_size; i++)
-			new[i] = old[i];
-	}
 	if (new == NULL)
 		return (NULL);
+	if (ptr == NULL)
+		return (new);
 	if (new_size >= old_size)
 	{
 		for (i = 0; i < old_size; i++)
