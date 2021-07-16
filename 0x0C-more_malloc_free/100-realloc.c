ffa2bbc9d2c30f3b5b884a4ebed4ffc000c7367d
getssh <gtamrat33@gmail.com>
2021-07-16 22:22:28 +0300

    condition corrected

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,8 +15,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	old = (char *)ptr;
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0)
-		return (NULL);
 
 	if (new_size == 0 && ptr != NULL)
 	{
