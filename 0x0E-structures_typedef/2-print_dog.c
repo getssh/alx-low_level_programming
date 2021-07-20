67150235afba3516655e75921ab8c5877f1d6379
getssh <gtamrat33@gmail.com>
2021-07-20 03:31:27 +0300

    null

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,17 +8,17 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\n");
+		if (d->age <= 0)
+			printf("Age: (nil)\n");
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
 
-	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	if (d->age <= 0)
-		printf("Age: (nil)\n");
-	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
+	}
 }
