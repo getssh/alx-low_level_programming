deaa7005c72bd8a49a4e55e5bcabff637fd2d37b
getssh <gtamrat33@gmail.com>
2021-08-02 18:31:02 +0300

    trying other way

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,33 @@
 #include "lists.h"
+#include "lists.h"
 /**
-  * add_node - add node at the begenning of a list
-  * @head: head of the list
-  * @str: string to be printed
-  * Return: address to the added node
-  */
+ * add_node - add node at the begenning of a list
+ * @head: head of the list
+ * @str: string to be printed
+ * Return: address to the added node
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *ptr = malloc(sizeof(list_t));
-	char *dupstr;
-	int length;
+	int len = 0;
+	list_t *new;
 
-	if (ptr == NULL)
-		return (NULL);
-	ptr->next = *head;
-	*head = ptr;
+	new = malloc(sizeof(list_t));
 
-	dupstr = strdup(str);
-	if (dupstr == NULL)
+	if (new == NULL)
 	{
-		free(ptr);
+		free(new->str);
 		return (NULL);
 	}
-	ptr->str = dupstr;
-	for (length = 0; dupstr[length] != '\0'; length++)
+	new->str = strdup(str);
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	for (len = 0; new->str[len] != '\0'; len++)
 		;
-	ptr->len = length;
+	new->len = len;
 
 	return (*head);
 }
+
+
