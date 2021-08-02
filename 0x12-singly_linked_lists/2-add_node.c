911ec82bfd6f18f5a01278d28515edccad9fa26e
getssh <gtamrat33@gmail.com>
2021-08-02 18:34:12 +0300

    free added

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,11 +14,13 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
 	{
 		free(new->str);
 		return (NULL);
 	}
-	new->str = strdup(str);
 	new->len = len;
 	new->next = *head;
 	*head = new;
