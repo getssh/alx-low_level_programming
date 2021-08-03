728adcfca683d6fdcc9ea150393804612aa8c466
getssh <gtamrat33@gmail.com>
2021-08-03 17:22:21 +0300

    now checkes if index is 0

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,7 +18,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		i++;
 		if (i == index)
-			return (head->next);
+		{
+			if (i == 0)
+				return (head);
+			else
+				return (head->next);
+		}
 		head = head->next;
 	}
 
