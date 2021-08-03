25159187119d7d54a35eff6696fe7a2498461e41
getssh <gtamrat33@gmail.com>
2021-08-03 23:55:34 +0300

    null fixed

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,7 +22,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		*head = traverse->next;
 		free(traverse);
-		traverse = NULL;
+		/*traverse = NULL;*/
 		return (1);
 	}
 	else
@@ -35,7 +35,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		}
 		prev->next = traverse->next;
 		free(traverse);
-		traverse = NULL;
 		return (1);
 	}
 
