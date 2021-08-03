fb02d303f19a2e688f4918f6f4ae938931e081f3
getssh <gtamrat33@gmail.com>
2021-08-03 23:34:45 +0300

    undo

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+	listint_t *head2;
 
-	while (temp != NULL)
+	head2 = *head;
+	temp = *head;
+	while (head2 != NULL)
 	{
+		temp = head2;
+		head2 = head2->next;
 		free(temp);
-		temp = temp->next;
+		*head = head2;
 	}
 	*head = NULL;
 }
