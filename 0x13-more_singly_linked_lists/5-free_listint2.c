eb34f4ad5c381bc0a391acadd47b35e9e83193ee
getssh <gtamrat33@gmail.com>
2021-08-03 15:02:43 +0300

    delete node n free node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,15 +9,12 @@ void free_listint2(listint_t **head)
 	listint_t *temp;
 	listint_t *head2;
 
-	head2 = *head;
 	while (head2 != NULL)
 	{
+		head2 = *head;
 		temp = head2->next;
-		free(head2);
+		free(*head);
 		head2 = temp;
 	}
-	temp = NULL;
-	head2 = NULL;
 	*head = NULL;
-	
 }
