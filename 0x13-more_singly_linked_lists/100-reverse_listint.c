ba3b783756bcfab8cb5f42bfdbba2cb9c6cb526b
getssh <gtamrat33@gmail.com>
2021-08-04 22:42:40 +0300

    reverse list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+  * reverse_listint - reverse a linked list
+  * @head: head of the list
+  * Return: the head of the reversed list
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *curr = *head;
+	listint_t *prev = NULL;
+	listint_t *nex = NULL;
+
+	if (*head == NULL || head == NULL)
+	{
+		free(*head);
+		return (NULL);
+	}
+
+	while (curr != NULL)
+	{
+		nex = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = nex;
+	}
+	*head = prev;
+
+	return (*head);
+}
