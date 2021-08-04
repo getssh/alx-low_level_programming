e104a86167ad7141c5487409973502c89f3cdc53
getssh <gtamrat33@gmail.com>
2021-08-04 23:40:06 +0300

    now only to vars

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,28 +1,26 @@
 #include "lists.h"
 /**
-  * reverse_listint - reverse a linked list
-  * @head: head of the list
-  * Return: the head of the reversed list
-  */
+ * reverse_listint - reverse a linked list
+ * @head: head of the list
+ * Return: the head of the reversed list
+ */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *curr = *head;
 	listint_t *prev = NULL;
 	listint_t *nex = NULL;
 
 	if (*head == NULL || head == NULL)
 	{
-		free(*head);
 		*head = NULL;
 		return (*head);
 	}
 
-	while (curr != NULL)
+	while (*head != NULL)
 	{
-		nex = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = nex;
+		nex = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = nex;
 	}
 	*head = prev;
 
