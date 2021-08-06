fa72becd68446dc80fd527dfe5f25ca95a284133
getssh <gtamrat33@gmail.com>
2021-08-07 01:58:13 +0300

    undo to the bit oprators

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,32 +1,17 @@
 #include "main.h"
 /**
-  * get_bit - change int to binary and return bit at index
-  * @n: given number
-  * @index: index of the bit to find
-  * Return: the bit at the given index or -1
-  */
+ * get_bit - Gets the value of a bit at a given index
+ * @n: The bit
+ * @index: The index to get the value
+ * Return: value of bit at index
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int res, i = 0;
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
 
-	res = int_to_bi(n);
+	if ((n & (1 << index)) == 0)
+		return (0);
 
-	while (res > 0)
-	{
-		if (i == index)
-			return (res % 10);
-		i++;
-		res /= 10;
-	}
-
-	return (-1);
-}
-/**
-  * int_to_bi - change int to binary
-  * @k: number to chnage
-  * Return: binary of k
-  */
-unsigned long int int_to_bi(unsigned long int k)
-{
-	return (k == 0 || k == 1 ? k : ((k % 2) + 10 * int_to_bi(k / 2)));
+	return (1);
 }
