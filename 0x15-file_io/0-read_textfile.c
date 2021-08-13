55dce343aa7e0b35bd2140cae7937002e30d3570
getssh <gtamrat33@gmail.com>
2021-08-14 00:59:23 +0300

    reading and printing to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+  * read_textfile - read and printout form a file
+  * @filename: nameof the file
+  * @letters: sizeof bytes to read and printout
+  * Return: the number of bytes that read
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int opn, red, wrt;
+	char *buff;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		free(buff);
+		return (0);
+	}
+	opn = open(filename, O_RDONLY);
+	red = read(opn, buff, letters);
+	if (opn == -1 || red == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	close(opn);
+	opn = open(filename, O_RDWR);
+	if (opn == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	red = read(opn, buff, letters);
+	wrt = write(1, buff, red);
+	if (wrt == -1 || wrt != letters)
+	{
+		free(buff);
+		return (0);
+	}
+	close(opn);
+
+	free(buff);
+	return (wrt);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,13 @@
+In this project file input and output are included
+mainly the system calls open, read, write
+the tasks included in this project are
+
+0-main.c
+0-read_textfile.c
+1-create_file.c
+1-main.c
+2-append_text_to_file.c
+2-main.c
+3-cp.c
+3-main.c
+main.h
