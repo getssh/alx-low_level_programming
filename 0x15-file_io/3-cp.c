d7f3800d0b008d3f945a07fd5c2373c845bca085
getssh <gtamrat33@gmail.com>
2021-08-14 20:32:07 +0300

    file mode and len dicreased

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,24 +21,23 @@ int main(int argc, char *argv[])
 	red = read(opn, buff, 1024);
 	if (opn == -1 || red == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", from);
+		dprintf(2, "Error: Can't read from %s\n", from);
 		exit(98);
 	}
 	cls = close(opn);
 	if (cls == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\b", opn);
+		dprintf(2, "Error: Can't close fd %d\b", opn);
 		exit(100);
 	}
-	opn2 = open(to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	opn2 = open(to, O_TRUNC | O_CREAT | O_WRONLY, 0664);
 	/*		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);*/
 	for (len = 0; buff[len] != '\0'; len++)
 		;
-	len++;
 	wrt = write(opn2, buff, len);
 	if (opn2 == -1 || wrt == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
+		dprintf(2, "Error: Can't write to %s\n", to);
 		exit(99);
 	}
 	cls2 = close(opn2);
