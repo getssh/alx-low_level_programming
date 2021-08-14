7e0e1f80de3e5d127d83b6c7e232cf27ba0040ff
getssh <gtamrat33@gmail.com>
2021-08-14 12:43:36 +0300

    checking if content is null

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,35 +9,24 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fil[3], len;
-	char *buff;
 
 	if (filename == NULL)
 		return (-1);
 	for (len = 0; text_content[len] != '\0'; len++)
 		;
-	len++;
-	buff = malloc(sizeof(char) * len);
-	if (buff == NULL)
-	{
-		free(buff);
-		return (-1);
-	}
 	fil[0] = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fil[0] == -1)
-	{
-		free(buff);
 		return (-1);
-	}
-	fil[1] = read(fil[0], buff, len);
-	close(fil[0]);
-	fil[0] = open(filename, O_RDWR);
-	write(fil[0], text_content, len);
-	if (fil[0] == -1)
+	if (text_content != NULL)
 	{
-		free(buff);
-		return (-1);
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		fil[1] = read(fil[0], text_content, len);
+		/*close(fil[0]);*/
+		write(fil[0], text_content, len);
+		if (fil[0] == -1 || fil[1] == -1)
+			return (-1);
 	}
 	close(fil[0]);
-	free(buff);
 	return (1);
 }
