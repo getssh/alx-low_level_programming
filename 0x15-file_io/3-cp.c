71d25b209615461c69512d987dddf7286319ef34
getssh <gtamrat33@gmail.com>
2021-08-14 19:52:41 +0300

    file mode and now print to symbolic dicriptor

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,30 +14,30 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(2, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 	opn = open(from, O_RDWR);
 	red = read(opn, buff, 1024);
 	if (opn == -1 || red == -1)
 	{
-		dprintf(2, "Error: Can't read from %s\n", from);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", from);
 		exit(98);
 	}
 	cls = close(opn);
 	if (cls == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\b", opn);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\b", opn);
 		exit(100);
 	}
-	opn2 = open(to, O_CREAT | O_RDWR | O_TRUNC | O_SYNC,
-			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	opn2 = open(to, O_CREAT | O_RDWR | O_TRUNC | O_SYNC, 0666);
+	/*		S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);*/
 	for (len = 0; buff[len] != '\0'; len++)
 		;
 	wrt = write(opn2, buff, len);
 	if (opn2 == -1 || wrt == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
 		exit(99);
 	}
 	cls2 = close(opn2);
