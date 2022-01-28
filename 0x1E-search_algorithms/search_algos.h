d80f34e8a3dba9c0b06f5e0e8887617cf342d9af
root <root@DESKTOP-F2BLBCH.localdomain>
2022-01-28 12:26:54 +0300

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,43 +3,8 @@
 
 #include <stddef.h>
 #include <stdio.h>
-#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
-
-/**
- * struct listint_s - singly linked list
- * @n: int value
- * @index: index of the node list
- * @next: pointer to the next node
- */
-typedef struct listint_s
-{
-		int n;
-		size_t index;
-		struct listint_s *next;
-} listint_t;
-
-/**
- * struct skiplist_s - Singly linked list
- * @n: int value
- * @index: index of node
- * @next: points to next node
- * @express: pointer to next node with express
- */
-typedef struct skiplist_s
-{
-				int n;
-				size_t index;
-				struct skiplist_s *next;
-				struct skiplist_s *express;
-} skiplist_t;
 
 #endif /*SEARCH_HEAD*/
