7ca9c97b33cf0e8a419f1dc231831202e385f57c
root <root@DESKTOP-F2BLBCH.localdomain>
2022-01-28 11:44:19 +0300

    searching values using binary algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+
+/**
+  * binary_search - Search value in an array
+  * @array: an array to be searched
+  * @size: size of the elements in the array
+  * @value: value to be searched
+  * Return: the index of the founded value
+  */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, left, right;
+
+	if (array == NULL)
+		return (-1);
+
+	for (left = 0, right = size - 1; right >= left;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
+		else
+			left = i + 1;
+	}
+
+	return (-1);
+}
